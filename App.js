addb5d5793cf2d5f81636b04e6caf3b18848c8d9
Aldrin Duites <aldrin092294@gmail.com>
2018-06-11 21:50:52 +0800

    Edited

--- App.js
+++ App.js
@@ -179,9 +179,7 @@ export default class App extends React.Component {
       </KeyboardAvoidingView>
 
       
-      // <View style={styles.container}>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      // </View>
+
     );
   }
 }
